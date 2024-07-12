28aef02e873d6f2111f6c82b918be48afc658207
Christina Giaramani <christinagiaramani@gmail.com>
2024-07-12 11:49:10 +0300

    Zoho verification

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/Root";
-import Homepage from "./pages/Homepage";
-import AuthPage, { authAction } from "./pages/AuthPage";
-import ErrorPage from "./pages/ErrorPage";
-import { tokenLoader } from "./utils/auth";
-import { logoutAction } from "./components/Logout";
-
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './pages/Root'
+import Homepage from './pages/Homepage'
+import AuthPage, { authAction } from './pages/AuthPage'
+import ErrorPage from './pages/ErrorPage'
+import { tokenLoader } from './utils/auth'
+import { logoutAction } from './components/Logout'
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        id: "root",
-        loader: tokenLoader,
-        children: [
-            { index: true, element: <Homepage /> },
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
+    children: [
+      { index: true, element: <Homepage /> },
 
-            {
-                path: 'auth',
-                element: <AuthPage />,
-                action: authAction
-            },
-            {
-                path: 'logout',
-                action: logoutAction
-            }
-        ]
-    }
+      {
+        path: 'auth',
+        element: <AuthPage />,
+        action: authAction,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
+      },
+    ],
+  },
 ])
 
-
 const App = () => {
-    return (
+  const path = window.location.pathname
+  console.log(path)
+  return (
+    <>
+      {path === '/zohoverify/verifyforzoho.html' ? (
+        '64931922'
+      ) : (
         <RouterProvider router={router} />
-    );
-};
+      )}
+    </>
+  )
+}
 
-export default App;
+export default App
