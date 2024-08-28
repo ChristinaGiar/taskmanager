92ddf94c22242e6668f10105bda569b2bf502e8e
Christina Giaramani <christinagiaramani@gmail.com>
2024-08-28 16:38:09 +0300

    .env created

--- src/utils/smtp.js
+++ src/utils/smtp.js
@@ -1,6 +1,6 @@
 import { createClient } from 'smtpexpress'
 
 export const smtpexpressClient = createClient({
-  projectId: 'sm0pid-3E2AdEaB2cI_Wd485D6nBlMnK',
-  projectSecret: '0ad3ced55b700382a6f610cc66225bd583c65fe5eab3db5795',
+  projectId: import.meta.env.EMAIL_PROJECT_ID,
+  projectSecret: import.meta.env.EMAIL_PROJECT_SECRET,
 })
